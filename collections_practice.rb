b89bea39b92f3eaba95383262f6f884b253a5233
Romano Livi <romano_livi@outlook.com>
2020-12-14 16:35:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(nums)
+  nums.sort 
+end
+
+def sort_array_desc(nums)
+  nums.sort.reverse
+end
+
+def sort_array_char_count(nums)
+  nums.sort.length
+end
+
+def swap_elements(nums)
+  nums.sort.swap
+end
+
+def reverse_array(nums)
+  nums.reverse
+end
+
+def kesha_maker
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
